a5e08d87966911a4353aa38731eeab5f716cef70
hugostudio <hugostudio@yahoo.com.br>
2018-10-28 17:02:15 -0300

    Validação customizada : Email não repetido na inserção de Cliente

--- src/main/java/com/hugo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/hugo/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hugo.cursomc.domain.Cliente;
 import com.hugo.cursomc.domain.enums.TipoCliente;
 import com.hugo.cursomc.dto.ClienteNewDTO;
+import com.hugo.cursomc.repositories.ClienteRepository;
 import com.hugo.cursomc.resources.exception.FieldMessage;
 import com.hugo.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente cliAux = clienteRepository.findByEmail(objDto.getEmail());
+		if(cliAux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
