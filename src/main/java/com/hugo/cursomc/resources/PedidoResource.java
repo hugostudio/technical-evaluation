ec25d1b8d3b5f73507c3367f838bbff17c24f939
Hugo Leonardo <hugostudio@yahoo.com.br>
2018-10-30 16:55:29 -0300

    Inserindo pedido

--- src/main/java/com/hugo/cursomc/resources/PedidoResource.java
+++ src/main/java/com/hugo/cursomc/resources/PedidoResource.java
@@ -1,13 +1,18 @@
 package com.hugo.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.hugo.cursomc.domain.Pedido;
 import com.hugo.cursomc.services.PedidoService;
@@ -29,5 +34,13 @@ public class PedidoResource {
 	public ResponseEntity<List<Pedido>> get() {
 		List<Pedido> obj = pedidoService.listar();
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {		
+		obj = pedidoService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/hugo/cursomc/services/PedidoService.java
+++ src/main/java/com/hugo/cursomc/services/PedidoService.java
@@ -1,12 +1,18 @@
 package com.hugo.cursomc.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hugo.cursomc.domain.ItemPedido;
+import com.hugo.cursomc.domain.PagamentoComBoleto;
 import com.hugo.cursomc.domain.Pedido;
+import com.hugo.cursomc.domain.enums.EstadoPagamento;
+import com.hugo.cursomc.repositories.ItemPedidoRepository;
+import com.hugo.cursomc.repositories.PagamentoRepository;
 import com.hugo.cursomc.repositories.PedidoRepository;
 import com.hugo.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,19 +20,54 @@ import com.hugo.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 
 	@Autowired
-	private PedidoRepository dedidoRepo;
+	private PedidoRepository pedidoRepo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido buscar(Integer id) {		
-		Optional<Pedido> obj = dedidoRepo.findById(id);
+		Optional<Pedido> obj = pedidoRepo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
 	public List<Pedido> listar() {		
-		List<Pedido> obj = dedidoRepo.findAll();
+		List<Pedido> obj = pedidoRepo.findAll();
 		if(obj == null) {
 			 throw new ObjectNotFoundException( "Objeto não encontrado ! , Tipo: " + Pedido.class.getName());
 		}
+		return obj;
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto ) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = pedidoRepo.save(obj);
+		
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.buscar(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		
 		return obj;
 	}
 }
