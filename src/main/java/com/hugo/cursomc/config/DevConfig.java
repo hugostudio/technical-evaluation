ea128c18713c5241ed5fc8700f10411df50b8784
PROVIDER <PROVIDER@PRORJ-0562>
2019-04-22 17:15:31 -0300

    Ajustes no profile de DEV

--- src/main/java/com/hugo/cursomc/config/DevConfig.java
+++ src/main/java/com/hugo/cursomc/config/DevConfig.java
@@ -1,8 +1,9 @@
-package com.hugo.cursomc;
+package com.hugo.cursomc.config;
 
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,20 +13,30 @@ import com.hugo.cursomc.services.EmailService;
 import com.hugo.cursomc.services.MockEmailService;
 
 @Configuration	
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		dbService.instantiateDatabase();
-		return true;
+		
+		if("create".equals(strategy)) {
+			dbService.instantiateDatabase();
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
-
+	
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+
 }
--- src/main/java/com/hugo/cursomc/config/TestConfig.java
+++ src/main/java/com/hugo/cursomc/config/TestConfig.java
@@ -1,35 +1,31 @@
-package com.hugo.cursomc;
+package com.hugo.cursomc.config;
 
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.hugo.cursomc.services.DBService;
+import com.hugo.cursomc.services.EmailService;
+import com.hugo.cursomc.services.MockEmailService;
 
 @Configuration	
-@Profile("dev")
-public class DevConfig {
+@Profile("test")
+public class TestConfig {
 	
 	@Autowired
 	private DBService dbService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
 
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
-		if("create".equals(strategy)) {
-			dbService.instantiateDatabase();
-			return true;
-		} else {
-			return false;
-		}
-		
+		dbService.instantiateDatabase();
+		return true;
 	}
 
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
 }
