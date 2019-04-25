e38bbaed5b5674356d6600ee5f0536c6226c7cd7
HLS0419 <HLS0419@TERCPE046A38.Sulamerica.br>
2019-04-25 16:33:35 -0300

    Ajustes

--- src/main/java/com/hugo/cursomc/config/TestConfig.java
+++ src/main/java/com/hugo/cursomc/config/TestConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.hugo.cursomc.services.DBService;
 import com.hugo.cursomc.services.EmailService;
-import com.hugo.cursomc.services.MockEmailService;
+import com.hugo.cursomc.services.SmtpEmailService;
 
 @Configuration	
 @Profile("test")
@@ -26,6 +26,6 @@ public class TestConfig {
 
 	@Bean
 	public EmailService emailService() {
-		return new MockEmailService();
+		return new SmtpEmailService();
 	}
 }
