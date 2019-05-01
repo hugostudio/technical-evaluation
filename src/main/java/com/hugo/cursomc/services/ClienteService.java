e4a37bb631758193671ee8d8929a4073105fb19a
hugostudio <hugostudio@hugostudio-300E5K-300E5Q>
2019-05-01 17:16:17 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/hugo/cursomc/services/ClienteService.java
+++ src/main/java/com/hugo/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.hugo.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -35,7 +37,11 @@ public class ClienteService {
 	@Autowired private BCryptPasswordEncoder passworEncoder;
 	@Autowired private ClienteRepository clienteRepo;
 	@Autowired private EnderecoRepository enderecoRepo;
-	@Autowired private S3Service s3Service;
+	@Autowired private S3Service s3Service;	
+	@Autowired private ImageService imageService;
+
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente buscar(Integer id) {		
 		
@@ -112,6 +118,14 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
