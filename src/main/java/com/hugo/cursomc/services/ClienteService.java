7effc53a3e96697e56ee50d23b40c0a4d3b94fdb
hugostudio <hugostudio@hugostudio-300E5K-300E5Q>
2019-05-01 12:57:00 -0300

    Enviando imagem via endpoint

--- src/main/java/com/hugo/cursomc/services/ClienteService.java
+++ src/main/java/com/hugo/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.hugo.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hugo.cursomc.domain.Cidade;
 import com.hugo.cursomc.domain.Cliente;
@@ -29,12 +31,11 @@ import com.hugo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	@Autowired
-	private BCryptPasswordEncoder passworEncoder;
-	@Autowired
-	private ClienteRepository clienteRepo;
-	@Autowired
-	private EnderecoRepository enderecoRepo;
+	
+	@Autowired private BCryptPasswordEncoder passworEncoder;
+	@Autowired private ClienteRepository clienteRepo;
+	@Autowired private EnderecoRepository enderecoRepo;
+	@Autowired private S3Service s3Service;
 	
 	public Cliente buscar(Integer id) {		
 		
@@ -70,10 +71,6 @@ public class ClienteService {
 		return clienteRepo.save(newObj);
 	}
 	
-	private void updateData(Cliente newObj, Cliente obj) {
-		newObj.setNome(obj.getNome());
-		newObj.setEmail(obj.getEmail());
-	}
 
 	public void delete(Integer id) {
 		buscar(id);
@@ -107,5 +104,14 @@ public class ClienteService {
 			cli.getTelefones().add(objDTO.getTelefone3());
 		}
 		return cli;
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
